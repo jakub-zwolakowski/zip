c153aaff84631e5d3e1faf8c6a191cf857dd907f
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-01-25 15:04:35 +0100

    IMPROVE stub.c

--- trustinsoft/stub.c
+++ trustinsoft/stub.c
@@ -9,19 +9,20 @@ time_t time(time_t *tloc) {
 int chmod(const char *pathname, mode_t mode) {
   return 0;
 }
-
-#include <stdio.h>
-
-FILE *freopen(const char *path, const char *mode, FILE *stream) {
-  if (stream != NULL)
-    fclose(stream);
-  stream = fopen(path, mode);
-  return stream;
+int mkdir(const char *path, mode_t mode) {
+  /* Upon successful completion, mkdir() shall return 0. */
+  return 0;
 }
 
-#include <unistd.h>
+// #include <stdio.h>
+//
+// FILE *freopen(const char *path, const char *mode, FILE *stream) {
+//   if (stream != NULL)
+//     fclose(stream);
+//   stream = fopen(path, mode);
+//   return stream;
+// }
 
-int rmdir(const char *path) {
-  /* Upon successful completion, the function rmdir() shall return 0. */
-  return 0;
+void tis_make_unknown(char *__p, unsigned long __l) {
+  return;
 }
