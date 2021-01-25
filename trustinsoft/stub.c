999b8b6dd487b9c38db850c6509302d6fce22c2f
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-01-25 15:30:04 +0100

    REMOVE all tests but one

--- trustinsoft/stub.c
+++ trustinsoft/stub.c
@@ -9,19 +9,11 @@ time_t time(time_t *tloc) {
 int chmod(const char *pathname, mode_t mode) {
   return 0;
 }
+
 int mkdir(const char *path, mode_t mode) {
   /* Upon successful completion, mkdir() shall return 0. */
   return 0;
 }
-
-// #include <stdio.h>
-//
-// FILE *freopen(const char *path, const char *mode, FILE *stream) {
-//   if (stream != NULL)
-//     fclose(stream);
-//   stream = fopen(path, mode);
-//   return stream;
-// }
 
 void tis_make_unknown(char *__p, unsigned long __l) {
   return;
