e2ab7b49ba2c481febd7d863f0fae8c537fbce1b
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-01-27 11:19:46 +0100

    FIX examples.c

--- trustinsoft/examples.c
+++ trustinsoft/examples.c
@@ -14,6 +14,8 @@ int create (void) {
     int result = 0;
 
     struct zip_t *zip = zip_open("foo.zip", ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
+    if (zip == NULL) return -1;
+
     {
         result = zip_entry_open(zip, "foo-1.txt");
         if (result != 0) goto create_done;
@@ -53,6 +55,8 @@ int append (void) {
     int result = 0;
 
     struct zip_t *zip = zip_open("foo.zip", ZIP_DEFAULT_COMPRESSION_LEVEL, 'a');
+    if (zip == NULL) return -1;
+
     {
         result = zip_entry_open(zip, "foo-3.txt");
         if (result != 0) goto append_done;
@@ -97,12 +101,17 @@ int extract_into_memory (void) {
     size_t bufsize;
 
     struct zip_t *zip = zip_open("foo.zip", 0, 'r');
+    if (zip == NULL) return -1;
+
     {
         result = zip_entry_open(zip, "foo-1.txt");
         if (result != 0) goto extract_into_memory_done;
         {
-            result = zip_entry_read(zip, &buf, &bufsize);
-            if (result != 0) goto extract_into_memory_done;
+            size_t read_size = zip_entry_read(zip, &buf, &bufsize);
+            if (read_size > 0)
+                printf("zip_entry_read size = %zu\n", read_size);
+            else
+                return -1;
         }
         result = zip_entry_close(zip);
         if (result != 0) goto extract_into_memory_done;
@@ -126,15 +135,21 @@ int extract_into_memory_no_allocation (void) {
     size_t bufsize;
 
     struct zip_t *zip = zip_open("foo.zip", 0, 'r');
+    if (zip == NULL) return -1;
+
     {
         result = zip_entry_open(zip, "foo-1.txt");
         if (result != 0) goto extract_into_memory_no_allocation_done;
         {
             bufsize = zip_entry_size(zip);
             buf = calloc(sizeof(unsigned char), bufsize);
+            if (buf == NULL) return -1;
 
-            result = zip_entry_noallocread(zip, (void *)buf, bufsize);
-            if (result != 0) goto extract_into_memory_no_allocation_done;
+            size_t read_size = zip_entry_noallocread(zip, (void *)buf, bufsize);
+            if (read_size > 0)
+                printf("zip_entry_noallocread size = %zu\n", read_size);
+            else
+                return -1;
         }
         result = zip_entry_close(zip);
         if (result != 0) goto extract_into_memory_no_allocation_done;
@@ -155,6 +170,8 @@ int extract_entry_into_a_file (void) {
     int result = 0;
 
     struct zip_t *zip = zip_open("foo.zip", 0, 'r');
+    if (zip == NULL) return -1;
+
     {
         result = zip_entry_open(zip, "foo-2.txt");
         if (result != 0) goto extract_entry_into_a_file_done;
@@ -179,7 +196,11 @@ int list_all_entries (void) {
     int result = 0;
 
     struct zip_t *zip = zip_open("foo.zip", 0, 'r');
+    if (zip == NULL) return -1;
+
     int i, n = zip_total_entries(zip);
+    if (n < 0) return n;
+
     for (i = 0; i < n; ++i) {
         result = zip_entry_openbyindex(zip, i);
         if (result != 0) goto list_all_entries_done;
@@ -207,6 +228,7 @@ int zip_walk(struct zip_t *zip, const char *path) {
     struct dirent *entry;
     char fullpath[MAX_PATH];
     struct stat s;
+    int result = 0;
 
     printf("zip_walk: BEGIN %s\n", path);
     memset(fullpath, 0, MAX_PATH);
@@ -222,20 +244,25 @@ int zip_walk(struct zip_t *zip, const char *path) {
       stat(fullpath, &s);
       if (S_ISDIR(s.st_mode)) {
         printf("zip_walk: WALK %s\n", fullpath);
-        zip_walk(zip, fullpath);
+        result = zip_walk(zip, fullpath);
+        if (result != 0) goto zip_walk_done;
       }
       else {
         printf("zip_walk: WRITE %s\n", fullpath);
-        zip_entry_open(zip, fullpath);
-        zip_entry_fwrite(zip, fullpath);
-        zip_entry_close(zip);
+        result = zip_entry_open(zip, fullpath);
+        if (result != 0) goto zip_walk_done;
+        result = zip_entry_fwrite(zip, fullpath);
+        if (result != 0) goto zip_walk_done;
+        result = zip_entry_close(zip);
+        if (result != 0) goto zip_walk_done;
       }
     }
 
     closedir(dir);
     printf("zip_walk: END %s\n", path);
 
-    return 0;
+zip_walk_done:
+    return result;
 }
 
 int compress_folder_recursively(void) {
